b3378d9b08a2ee55f9f2ac861731de4f6c3fbd6f
jch-berrezueta <jonatgfhan@gmail.com>
2026-01-08 01:31:58 -0500

    :)

---
+++ src/app/features/admin/bodega/pages/entregas/form/form.component.ts
@@ -0,0 +1,177 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UiTextFieldComponent } from "@shared/components/text-field/text-field.component";
+import { UiButtonComponent } from "@shared/components/button/button.component";
+import { ActivatedRoute } from '@angular/router';
+import { UiDatetimePickerComponent } from "@shared/components/datetime-picker/datetime-picker.component";
+import { FormGroupOf } from '@core/utils/utilities';
+import { UiTextAreaComponent } from "@shared/components/text-area/text-area.component";
+import { IEmpresa, IEmpresaResult } from '@models';
+import { UiComboBoxComponent } from '@shared/components/combo-box/combo-box.component';
+import { IComboBoxOption } from '@shared/models/combo_box_option';
+import Swal from 'sweetalert2'
+import { Location } from '@angular/common'; // 1. Importar Location
+import { EmpresasService } from '@services/index';
+
+const IMPORTS = [
+  UiTextFieldComponent, 
+  UiTextAreaComponent,
+  UiDatetimePickerComponent,
+  UiComboBoxComponent,
+  UiButtonComponent,
+  ReactiveFormsModule, 
+];
+
+type EmpresaFormGroup = FormGroupOf<IEmpresa>;
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: IMPORTS,
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export default class FormComponent {
+  
+  protected estadosEmpresa!: IComboBoxOption[];
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _empresasService = inject(EmpresasService);
+  private readonly formBuilder = inject(FormBuilder);
+  public location = inject(Location);
+  protected formData!: EmpresaFormGroup;
+  private initialFormValue!: IEmpresa;
+  protected isAdd: boolean = true;
+  private idParam: number = -1;
+
+  constructor() {
+    this.loadEstadosEmpresa();
+  }
+
+  ngOnInit() {
+    const idParam = this._route.snapshot.params['id'];
+    this.initForm();
+    if(idParam){
+      this.setData(idParam);
+    }
+  }
+
+  private initForm(): void {
+    this.formData = this.formBuilder.group({
+      ideEmp: [{ value: -1, disabled: true }, [Validators.required]],        
+      nombreEmp: ['', [Validators.required], []],
+      responsableEmp: ['', [Validators.required], []],
+      fechaContratoEmp: ['', [Validators.required], []],
+      direccionEmp: ['', [Validators.required], []],
+      telefonoEmp: ['', [Validators.required], []],
+      emailEmp: ['', [Validators.required], []],
+      estadoEmp: ['', [Validators.required], []],
+      descripcionEmp: ['', [Validators.required], []]
+    }) as EmpresaFormGroup;
+
+    // snapshot inicial
+    this.initialFormValue = this.formData.getRawValue();
+  }
+
+  private setData(idParam: number) {
+    this._empresasService.buscar(idParam).subscribe(
+      (res) => {
+        const empresa = res.data[0] as IEmpresaResult;
+        this.idParam = empresa.ide_empr;
+        this.isAdd = false;
+        this.formData.patchValue({
+          ideEmp: empresa.ide_empr,
+          nombreEmp: empresa.nombre_empr,
+          responsableEmp: empresa.responsable_empr,
+          fechaContratoEmp: empresa.fecha_contrato_empr,
+          direccionEmp: empresa.direccion_empr,
+          telefonoEmp: empresa.telefono_empr,
+          emailEmp: empresa.email_empr,
+          estadoEmp: empresa.estado_empr,
+          descripcionEmp: empresa.descripcion_empr
+        });
+      }
+    )
+  }
+
+  private loadEstadosEmpresa() {
+    this._empresasService.listarEstados().subscribe(
+      (res) => {
+        this.estadosEmpresa = res;
+      }
+    );
+  }
+
+  protected guardar(): void {
+    if(!this.formData.invalid){
+      const data = this.formData.getRawValue() as IEmpresa;
+      if(this.isAdd){
+        data.ideEmp = -1;
+        this._empresasService.insertar(data).subscribe(
+          (res) => {
+            Swal.fire({
+              title: "Empresa registrada :)",
+              text: "La empresa fue guardada correctamente",
+              icon: "success"
+            });
+            this.location.back();
+            this.resetForm();
+          }
+        );
+      }else{
+        Swal.fire({
+          title: "Esta seguro de modificar esta empresa?",
+          text: "Los cambios realizados se registraran!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si, de acuerdo"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            data.ideEmp = this.idParam;
+            this._empresasService.actualizar(this.idParam, data).subscribe(
+              (res) => {
+                Swal.fire({
+                  title: "Empresa actualizada :)",
+                  text: "La empresa fue actualizada correctamente",
+                  icon: "success"
+                });
+                this.location.back();
+                this.resetForm();
+              }
+            );
+          }
+        });
+        
+      }
+    }else{
+      Swal.fire({
+        icon: "info",
+        title: "Oops... Faltan datos",
+        text: "Revise porfavor la información ingresada"
+      });
+    }
+  }
+
+  protected cancelar(): void {
+    Swal.fire({
+      title: "Esta Seguro de Cancelar?",
+      text: "Los cambios realizados no se guardaran!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Cancelar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.resetForm();
+        this.location.back();
+      }
+    });
+    
+  }
+
+  protected resetForm() {
+    this.formData.reset(this.initialFormValue);
+  }
+}
---
+++ src/app/features/admin/bodega/pages/pedidos/list/list_pedidos.config.ts
@@ -0,0 +1,98 @@
+import { ITableListConfig } from '@shared/models/table-list.model';
+
+export const PedidoConfig: ITableListConfig = {
+  dataKey: 'ide_pedi',
+  columns: [
+    {
+      label: 'Detalles',
+      property: 'view_details',
+      type: 'details',
+      buttonItems: [
+        {
+          action: 'details',
+          label: 'Ver Detalles',
+          icon: 'visibility',
+          router: true,
+          key: 'ide_pedi',
+          color: 'purple'
+        }
+      ]
+    },
+    {
+      label: 'ID',
+      property: 'ide_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Empresa',
+      property: 'nombre_empr',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Fecha Pedido',
+      property: 'fecha_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Fecha Entrega',
+      property: 'fecha_entr_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Cantidad Total',
+      property: 'cantidad_total_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Total',
+      property: 'total_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Estado',
+      property: 'estado_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Motivo',
+      property: 'motivo_pedi',
+      type: 'text',
+      sortable: true
+    },
+    {
+      label: 'Observación',
+      property: 'observacion_pedi',
+      type: 'text',
+      sortable: false
+    },
+    {
+      label: '',
+      property: 'menu',
+      type: 'ud',
+      buttonItems: [
+        {
+          action: 'update',
+          label: 'Editar',
+          icon: 'edit',
+          router: true,
+          key: 'ide_pedi',
+          color: 'primary'
+        },
+        {
+          action: 'delete',
+          label: 'Eliminar',
+          icon: 'delete',
+          key: 'ide_pedi',
+          color: 'red'
+        }
+      ]
+    }
+  ]
+};
