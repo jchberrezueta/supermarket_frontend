fed725c5775d960480668f17e0fa2e4afd610b75
jch-berrezueta <jonatgfhan@gmail.com>
2026-01-08 13:30:03 -0500

    :)

--- src/app/features/admin/ventas/pages/ventas/ventas.routes.ts
+++ src/app/features/admin/ventas/pages/ventas/ventas.routes.ts
@@ -1,23 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const ventasRoutes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'list'
-    },
-    {
-        path: 'list',
-        loadComponent: () => import('./list/list.component'),
-    },
-    /*{
-        path: 'edit',
-        loadComponent: () => import('./edit/edit.component'),
-    },*/
-    /*{
-        path: 'details',
-        loadComponent: () => import('./details/details.component'),
-    }*/
+    
 ];
 
 export default ventasRoutes;
