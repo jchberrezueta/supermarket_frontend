4de6dcb9bd86db33b817eb055de200bfcea06cc7
jch-berrezueta <jonatgfhan@gmail.com>
2026-06-13 13:06:12 -0500

    [Seguimos :)]

--- src/app/core/guards/permiso.guard.ts
+++ src/app/core/guards/permiso.guard.ts
@@ -1,40 +1,42 @@
 import { inject } from '@angular/core';
-import {
-  Router,
-  CanMatchFn,
-  UrlSegment
-} from '@angular/router';
+import { Router, CanMatchFn, UrlSegment } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 
-export const canMatchPermisoGuard: CanMatchFn  = (route, segments) => {
+export const canMatchPermisoGuard: CanMatchFn = (route, segments) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  
+
   const loginState = authService.isAuthenticated();
   if (!loginState) {
     return router.createUrlTree(['/auth/login']);
     //return false;
   }
-  
+
   const nav = router.getCurrentNavigation();
   const allSegments = nav?.initialUrl.root.children['primary']?.segments ?? [];
-  const faltantes = segments.map(s => s.path);
-  
-  if(route.path === 'list' && allSegments[allSegments.length-1].path != 'list'){
+  const faltantes = segments.map((s) => s.path);
+
+  if (
+    route.path === 'list' &&
+    allSegments[allSegments.length - 1].path != 'list'
+  ) {
     allSegments.push(new UrlSegment(faltantes[0], {}));
-  } 
-  
-  const rutaActual = '/' + allSegments
-    .slice(0, allSegments.length - faltantes.length)
-    .map(s => s.path)
-    .join('/');
+  }
+
+  const rutaActual =
+    '/' +
+    allSegments
+      .slice(0, allSegments.length - faltantes.length)
+      .map((s) => s.path)
+      .join('/');
 
   if (route.path === 'list') return authService.canList(rutaActual);
+  if (route.path === 'etiquetas') return authService.canList(rutaActual);
   if (route.path === 'insert') return authService.canInsert(rutaActual);
   if (route.path === 'details/:id') return authService.canList(rutaActual);
   if (route.path === 'update/:id') return authService.canUpdate(rutaActual);
   // debes poder todo para hacer algo extra sobre un item :)
-  if(route.path?.includes('/:id')) return authService.canAll(rutaActual);
+  if (route.path?.includes('/:id')) return authService.canAll(rutaActual);
 
   return false;
 };
